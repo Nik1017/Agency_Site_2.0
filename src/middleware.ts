87ee645de24ddcbca7ef5dcaf905a56e2a9e43b0
Nikhil Chandrakar <nikhilchandrakar00@gmail.com>
2026-08-09 23:35:53 +0530

    remover err from middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,15 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth/auth.config";
-import { type NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/middleware";
 
-const nextAuthMiddleware = NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
 
-export default async function middleware(request: NextRequest, event: any) {
-  // 1. Initialize Supabase middleware client
-  const supabaseResponse = createClient(request);
-
-  // 2. Run NextAuth middleware
-  const authResponse = await nextAuthMiddleware(request, event);
-
-  if (authResponse) {
-    // If NextAuth returns a response (like redirect), copy Supabase cookies to it
-    supabaseResponse.headers.forEach((value, key) => {
-      if (key.toLowerCase() === 'set-cookie') {
-        authResponse.headers.append(key, value);
-      }
-    });
-    return authResponse;
-  }
+export default auth(async (req) => {
+  const supabaseResponse = createClient(req as NextRequest);
 
   return supabaseResponse;
-}
+});
 
 export const config = {
   matcher: [
